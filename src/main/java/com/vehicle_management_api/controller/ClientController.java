fe3e0ceb37513aa24204f562265cf526aa7e71f3
PedroOSPaiva <pedrohenriqueospaiva@gmail.com>
2025-10-01 21:53:06 -0300

    refactor(ClientController):  validação em métodos

--- src/main/java/com/vehicle_management_api/controller/ClientController.java
+++ src/main/java/com/vehicle_management_api/controller/ClientController.java
@@ -8,14 +8,13 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.jar.JarVerifier;
+
 
 @RestController
 @RequestMapping("/admin/clients")
@@ -26,8 +25,11 @@ public class ClientController {
 
     private static final Logger logger = LogManager.getLogger(ClientController.class);
 
-    @Autowired
-    private ClientService;
+    private final ClientService clientService;
+
+    public ClientController(ClientService clientService){
+        this.clientService = clientService;
+    }
 
     @GetMapping
     @Operation(summary = "Obter todos os clientes (Somente administrador)")
